aad40d0673586ff7cc5b249e3462b5a5089e30ed
Mattia Cantalù <mattiacantalu@me.com>
2016-12-24 15:11:28 +0100

    Initial Commit

---
+++ streamingIPCamera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  streamingIPCamera
+//
+//  Created by Mattia Cantalù on 24/12/2016.
+//  Copyright © 2016 Mattia Cantalù. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
